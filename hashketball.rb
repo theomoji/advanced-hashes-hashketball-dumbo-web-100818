b383dcaa72257d5990f19dfcaecddcc9a3a05d85
Shuo Wang <gken90@gmail.com>
2018-09-14 21:55:19 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -1,3 +1,4 @@
+require "pry"
 def game_hash
   game_hash = {
   :home => {
@@ -117,18 +118,6 @@ def game_hash
 end
 
 def num_points_scored(name)
-  game_hash.each do |game, data|
-    binding.pry
-    data.each do |details, score|
 
-    end
-  end
 end
 # expect(num_points_scored("Jeff Adrien")).to eq(10)
-
-# def home_team_name
-#   game_hash[:home][:team_name]
-# end
-#
-# puts home_team_name
-# => "Brooklyn Nets"
